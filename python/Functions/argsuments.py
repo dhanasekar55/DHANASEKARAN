74bdb2a56885064867d557ee2cb6599dad8f2db4
dhanasekaran55 <153163826+dhanasekaran55@users.noreply.github.com>
2023-12-21 21:19:03 +0530

    Add files via upload

---
+++ python/Functions/argsuments.py
@@ -0,0 +1,5 @@
+def both(*args,**kwargs):
+  print(args)
+  print(kwargs)
+
+both(1,2,3,name="dhana",lname="rafi")
---
+++ python/Variables/newfiles.py
@@ -0,0 +1,4 @@
+name="Dhana"
+course="python 10.11"
+print("my name is",name)
+print(f"my course is ",{course})
